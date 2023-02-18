677d3d5d8ff6016a3c32af6f8f22d8999ce863d2
Hassan Nadeem <64688959+HassanNadeemWebD@users.noreply.github.com>
2023-02-18 15:12:16 +0500

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 
 
-const Form = () => {    
+const Form = () => {
     const [value, setValue] = useState("");
+    const [count, setCount] = useState(0);
     // setValue("abc")
-    const handleChange = (e)=>{
+    const handleChange = (e) => {
         // console.log(e.target.value)
 
-setValue(e.target.value)
+        setValue(e.target.value)
+        setCount(count+1)
+
 
     }
     return (
@@ -16,13 +19,14 @@ setValue(e.target.value)
             <form className="container">
                 <div className="form-group">
                     <label for="exampleFormControlInput1">Put Your Text Here</label>
-                    <input type="email" className="form-control" id="inputTxt"  value={value}  onChange={handleChange} />
+                    <input type="email" className="form-control" id="inputTxt" value={value} onChange={handleChange} />
                 </div>
 
                 <div className="form-group">
                     <label for="exampleFormControlTextarea1">Example textarea</label>
                     <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={value}></textarea>
                 </div>
+                <h2>{count}</h2>
             </form>
 
             {/* {document.getElementById("inputTxt")} */}
